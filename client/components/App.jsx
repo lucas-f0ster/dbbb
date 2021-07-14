0a6860a193068426dbed73629848cc1d6e6c878c
samkc-0 <81603006+samkc-0@users.noreply.github.com>
2021-07-14 23:24:52 +1200

    Initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+
+import { fetchFruits } from '../actions'
+
+function App (props) {
+  useEffect(() => {
+    props.dispatch(fetchFruits())
+  }, [])
+
+  return (
+    <>
+      <h1>DBB</h1>
+      <div className='app'>
+      </div>
+    </>
+  )
+}
+const mapStateToProps = (globalState) => {
+  return {
+    fruits: globalState.fruits
+  }
+}
+
+export default connect(mapStateToProps)(App)
