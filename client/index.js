1bf235842dbb0feab00a5b2adbaaf6bfd7e02e75
Edwyn McMillan <codemcmillan@gmail.com>
2021-07-14 23:42:37 +1200

    controller working, half working for 2p

--- client/index.js
+++ client/index.js
@@ -9,6 +9,9 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  input: {
+    gamepad: true
+  },
   physics: {
     default: "arcade",
     arcade: {
@@ -24,6 +27,7 @@ var config = {
 };
 
 var player;
+var player2;
 var stars;
 var bombs;
 var platforms;
@@ -31,6 +35,7 @@ var cursors;
 var score = 0;
 var gameOver = false;
 var scoreText;
+var gamepad
 
 var game = new Phaser.Game(config);
 
@@ -57,6 +62,16 @@ function preload() {
 }
 
 function create() {
+  //gamepad
+  this.input.gamepad.once('down', function (pad, button, index) {
+
+    console.log('Playing with ' + pad.id);
+
+    gamepad = pad;
+
+  }, this);
+
+
   //  A simple background for our game
   this.add.image(400, 300, "sky");
 
@@ -74,11 +89,13 @@ function create() {
 
   // The player and its settings
   player = this.physics.add.sprite(100, 450, "dude");
+  player2 = this.physics.add.sprite(100, 450, "dude");
 
   //  Player physics properties. Give the little guy a slight bounce.
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
-
+  player2.setBounce(0.2);
+  player2.setCollideWorldBounds(true);
   //  Our player animations, turning, walking left and walking right.
   this.anims.create({
     key: "left",
@@ -128,10 +145,16 @@ function create() {
   this.physics.add.collider(stars, platforms);
   this.physics.add.collider(bombs, platforms);
 
+  this.physics.add.collider(player2, platforms);
+
+
   //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
   this.physics.add.overlap(player, stars, collectStar, null, this);
+  this.physics.add.overlap(player2, stars, collectStar, null, this);
 
   this.physics.add.collider(player, bombs, hitBomb, null, this);
+  this.physics.add.collider(player2, bombs, hitBomb, null, this);
+
 }
 
 function update() {
@@ -139,22 +162,24 @@ function update() {
     return;
   }
 
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-
-    player.anims.play("left", true);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-
-    player.anims.play("right", true);
-  } else {
-    player.setVelocityX(0);
-
-    player.anims.play("turn");
-  }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
+  if(gamepad){
+    if (gamepad.left) {
+      player.setVelocityX(-160);
+  
+      player.anims.play("left", true);
+    } else if (gamepad.right) {
+      player.setVelocityX(160);
+  
+      player.anims.play("right", true);
+    } else {
+      player.setVelocityX(0);
+  
+      player.anims.play("turn");
+    }
+  
+    if (gamepad.up && player.body.touching.down) {
+      player.setVelocityY(-330);
+    }
   }
 }
 
