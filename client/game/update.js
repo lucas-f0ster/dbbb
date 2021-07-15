b4cacbd6ed23192ad47678e710433c205d20d91e
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-07-15 18:24:54 +1200

    added initial dash inputs - work in progress

--- client/game/update.js
+++ client/game/update.js
@@ -5,21 +5,49 @@ export default function update () {
     return
   }
 
-  if (game.cursors.left.isDown) {
-    game.player.setVelocityX(-160)
+// ****** Normal player movement ******
+    if (game.cursors.left.isDown) {
+      game.player.setVelocityX(-160)
+      game.player.anims.play('left', true)
 
-    game.player.anims.play('left', true)
-  } else if (game.cursors.right.isDown) {
-    game.player.setVelocityX(160)
+    } else if (game.cursors.right.isDown) {
+      game.player.setVelocityX(160)
+      game.player.anims.play('right', true)
 
-    game.player.anims.play('right', true)
-  } else {
-    game.player.setVelocityX(0)
+    } else {
+      game.player.setVelocityX(0)
+      game.player.anims.play('turn')
 
-    game.player.anims.play('turn')
-  }
+    }
 
-  if (game.cursors.up.isDown && game.player.body.touching.down) {
-    game.player.setVelocityY(-330)
-  }
+    if (game.cursors.up.isDown && game.player.body.touching.down) {
+      game.player.setVelocityY(-330)
+      
+    }
+ // ****** Normal player movement ******
+
+
+ // ******* Dash testing *******
+    // Dash needs a time out - currently runs until key is released 
+    // Dash needs to "stun" player if world edge or platform is hit
+    // Player still has control of movement while in dash
+    // No win condition is set
+    if (game.cursors.space.isDown && cursors.left.isDown){
+        dash = -1500
+        player.body.velocity.x = dash
+
+    } else if (game.cursors.space.isDown && cursors.right.isDown) {
+        dash = 1500
+        player.body.velocity.x = dash
+
+    } else if (game.cursors.space.isDown && cursors.up.isDown){
+        dash = -1500
+        player.body.velocity.y = dash
+
+    } else if (game.cursors.space.isDown && cursors.down.isDown){
+        dash = 1500
+        player.body.velocity.y = dash
+
+    }
+// ******* Dash testing *******
 }
